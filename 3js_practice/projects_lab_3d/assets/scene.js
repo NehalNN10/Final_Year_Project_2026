3ebf4cb91edcc92d6fb213b1a19ce5ec5676ca33
neeyazie <shawaizniazi917@gmail.com>
2026-01-12 01:55:26 +0500

    after hours

--- 3js_practice/projects_lab_3d/assets/scene.js
+++ 3js_practice/projects_lab_3d/assets/scene.js
@@ -11,7 +11,7 @@ document.body.appendChild(renderer.domElement);
 
 const fov = 75;
 const aspect = w / h;
-const near = 1;
+const near = 0.1;
 const far = 200;
 export const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.set(-10, 15);
