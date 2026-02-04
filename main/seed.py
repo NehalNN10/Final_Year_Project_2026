d74353a839a3fa921b9d2ebf8abb7a6b75bc9cdd
neeyazie <shawaizniazi917@gmail.com>
2026-02-04 17:45:24 +0500

    started shifting data to MongoDB

--- main/seed.py
+++ main/seed.py
@@ -1,5 +1,5 @@
 from app import app
-from models import db, User, Role
+from models import db, User, Role, Rooms, SecurityEmails
 from werkzeug.security import generate_password_hash
 
 # Connect to the app context to access the DB
@@ -11,8 +11,9 @@ with app.app_context():
     print("Seeding Database...")
 
     Role(name='Security Admin', department='Security', facilities_email=False).save()
-    Role(name='Facilities Admin', department='Facilities', facilities_email=False).save()
+    Role(name='Facilities Admin', department='Facilities', facilities_email=True).save()
     Role(name='Admin', department='Admin', facilities_email=True).save()
+    Role(name='Security Officer', department='Security', facilities_email=False).save()
     
     User(
         user_id='security',
@@ -34,5 +35,23 @@ with app.app_context():
         password=generate_password_hash('admin123'),
         role=Role.objects(name='Admin').first()
     ).save()
+
+    User(
+        user_id='nh07884',
+        email='nh07884@st.habib.edu.pk',
+        password=generate_password_hash('YoGurt67'),
+        role=Role.objects(name='Security Officer').first()
+    ).save()
+
+    Rooms(
+        room_id='C-007',
+        room_name='Projects Lab',
+        max_occupancy=50
+    )
+
+    SecurityEmails(
+        room='C-007',
+        user=User.objects(user_id='nh07884').first()
+    )
 
     print("Database seeded successfully!")
