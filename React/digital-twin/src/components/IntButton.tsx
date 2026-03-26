8ca1a624b15bf4ccab117d0ffe71880606317e43
neeyazie <shawaizniazi917@gmail.com>
2026-03-27 04:41:09 +0500

    more ui updates again

--- React/digital-twin/src/components/IntButton.tsx
+++ React/digital-twin/src/components/IntButton.tsx
@@ -11,7 +11,7 @@ interface IntButtonProps {
 export default function IntButton ({ icon: Icon, label, onClick, isActive = false, classes = "" }: IntButtonProps){
   return (
     <button className={`group ${isActive ? "selected" : ""} ${classes}`} onClick={onClick}>
-      <Icon size="20" />
+      <Icon size="20"  />
       <span className={`btn-base ${isActive ? "btn-active" : "btn-inactive"}`}> {label}</span>
     </button>
   );
